696cb553a2ee0d40dd47f11630cee6b74653a08a
efreiberg <eric.freiberg@pointsource.com>
2017-06-18 16:40:42 -0500

    Ignore dirs when discovering injectable modules

--- test/unit/discovery/test.js
+++ test/unit/discovery/test.js
@@ -1,8 +1,8 @@
 const assert = require('chai').assert;
-let discoveryList = require('../../../lib/discovery')(__dirname);
+let discoveryList = require('../../../lib/discovery')({}, __dirname);
 
 describe("Injectable Discovery Test", function () {
-    it("Find injectable modules, duplicates included", function () {
+    it("Find injectable modules, duplicates included, proper modules ignored", function () {
         assert.deepEqual(discoveryList.map(module => module.name).sort(), ['Service', 'Test', 'Test'])
     });
 });
